5eec08e9afcdb71dea6650984bb0168fb7feae54
Andrew Vitovitch <alvitovitch@gmail.com>
2021-12-02 19:12:54 -0500

    from black to blue, progress is made

--- src/index.js
+++ src/index.js
@@ -1,51 +1,59 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 
 // scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(255,0,0)
+
+scene.background = new THREE.Color(255,255,255)
 
 //loader
 const loader = new GLTFLoader();
 
-// loading tree
-loader.load("./src/Assets/tree/scene.gltf", function(gltf) {
+//loading tree
+loader.load("./src/Assets/tree/scene.glb", function(gltf) {
     const tree = gltf.scene;
-    console.log(tree)
-    tree.scale.set(0.5, 0.5, 0.5)
-    scene.add(tree)
-}, undefined,
-function(error){
-    console.log("An error occured")
+    scene.add(tree.children[0]);
+    }, undefined,
+    function(error){
+        console.log("An error occured")
 })
 
 // light
-const light = new THREE.AmbientLight(0x404040);
+const light = new THREE.AmbientLight(255,255,255);
 scene.add(light)
 
+// camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set( 5, 5, 5 );
+//camera.lookAt( 0, 0, 0 );
+
 // renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(0xEEEEEE);
-renderer.setSize( 1200, 900);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
+console.log(scene)
+console.log(camera)
+console.log(light)
+console.log(renderer)
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 100, 100, 100 );
-camera.lookAt( 0, 0, 0 );
+// cube
 
+const box = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.
+    MeshBasicMaterial({ color: (255,0,0)})
 
-var controls = new OrbitControls(camera, renderer.domElement)
-
-
-
-
+const cube = new THREE.Mesh(box, material)
+const cube2 = new THREE.Mesh(box, material)
+cube2.position
+scene.add(cube)
+scene.add(cube2)
 
 const animate = function () {
     requestAnimationFrame(animate);
-    controls.update()
+    cube.rotation.x += .01
+    cube2.rotation.y -=.01
     renderer.render(scene, camera)
 };
 animate();
